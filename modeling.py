0ebc1dc538c3d7478c3ae8059036071344463629
shun <w2310751769@gmail.com>
2022-06-01 11:36:37 +0000

    line on concatenation

--- modeling.py
+++ modeling.py
@@ -414,15 +414,15 @@ class AutoModelForSequenceClassification_SPV_MIP(nn.Module):
         self.config = config
         self.dropout = nn.Dropout(args.drop_ratio)
         self.args = args
-        self.basic_hidden_size = int(config.hidden_size/2)
+        self.basic_hidden_size = config.hidden_size
 
         self.SPV_linear = nn.Linear(config.hidden_size * 2, args.classifier_hidden)
         self.MIP_linear = nn.Linear(config.hidden_size * 2, args.classifier_hidden)
         self._init_weights(self.SPV_linear)
         self._init_weights(self.MIP_linear)
-        self.Basic_feature = nn.Linear(config.hidden_size * 2, config.hidden_size)
-        self.Basic_linear = nn.Linear(config.hidden_size, self.basic_hidden_size)
-        self._init_weights(self.Basic_feature)
+        #self.Basic_feature = nn.Linear(config.hidden_size * 2, config.hidden_size)
+        self.Basic_linear = nn.Linear(config.hidden_size*2, self.basic_hidden_size)
+        #self._init_weights(self.Basic_feature)
         self._init_weights(self.Basic_linear)
 
         self.logsoftmax = nn.LogSoftmax(dim=1)
@@ -517,9 +517,9 @@ class AutoModelForSequenceClassification_SPV_MIP(nn.Module):
         basic_target = basic_target.mean(1)
         con_target = con_target.mean(1)
         basic_MIP_hidden = self.dropout(torch.cat([basic_target, con_target], dim=1))
-        basic_MIP_hidden = self.SPV_linear(basic_MIP_hidden)
+        #basic_MIP_hidden = self.SPV_linear(basic_MIP_hidden)
 
-        basic_MIP_hidden = self.dropout(basic_MIP_hidden)
+        #basic_MIP_hidden = self.dropout(basic_MIP_hidden)
         basic_hidden = self.Basic_linear(basic_MIP_hidden)
         '''
         print(basic_MIP_hidden.shape)
